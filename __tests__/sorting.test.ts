3745ae2598a3f56e15d28ec71d43b70bee19b55f
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-04-03 23:33:29 -0300

    delete a duplicate data and refactor tests sorting

--- __tests__/sorting.test.ts
+++ __tests__/sorting.test.ts
@@ -1,37 +1,49 @@
 import { sortingBooks } from "../src/utils/sorting";
 
 describe('Sorting Function', () => {
-  const booksData = [
-    { id: 1, title: 'Z Book', price: 10 },
-    { id: 2, title: 'A Book', price: 5 },
-    { id: 3, title: 'M Book', price: 15 },
+  const bookMock = [
+    {
+      id: 1,
+      title: "Harry Potter",
+      price: 39.90
+    },
+    {
+      id: 2,
+      title: "Dom Casmurro",
+      price: 29.90
+    },
+    {
+      id: 3,
+      title: "The Lord of the Rings: The Fellowship of the Ring",
+      price: 59.90
+    },
   ];
 
-  it('should sort books A-Z', () => {
-    const sortedBooks = sortingBooks('A-Z', booksData);
-    expect(sortedBooks[0].title).toBe('A Book');
-    expect(sortedBooks[1].title).toBe('M Book');
-    expect(sortedBooks[2].title).toBe('Z Book');
+  it('should sort books A-Z by title', () => {
+    const sortedBooks = sortingBooks('A-Z', bookMock);
+    expect(sortedBooks[0].title).toBe('Dom Casmurro');
+    expect(sortedBooks[1].title).toBe('Harry Potter');
+    expect(sortedBooks[2].title).toBe('The Lord of the Rings: The Fellowship of the Ring');
   });
 
-  it('should sort books Z-A', () => {
-    const sortedBooks = sortingBooks('Z-A', booksData);
-    expect(sortedBooks[0].title).toBe('Z Book');
-    expect(sortedBooks[1].title).toBe('M Book');
-    expect(sortedBooks[2].title).toBe('A Book');
+  it('should sort books Z-A by title', () => {
+    const sortedBooks = sortingBooks('Z-A', bookMock);
+    expect(sortedBooks[0].title).toBe('The Lord of the Rings: The Fellowship of the Ring');
+    expect(sortedBooks[1].title).toBe('Harry Potter');
+    expect(sortedBooks[2].title).toBe('Dom Casmurro');
   });
 
   it('should sort books by price from lowest to highest', () => {
-    const sortedBooks = sortingBooks('moreCheaper', booksData);
-    expect(sortedBooks[0].price).toBe(5);
-    expect(sortedBooks[1].price).toBe(10);
-    expect(sortedBooks[2].price).toBe(15);
+    const sortedBooks = sortingBooks('moreCheaper', bookMock);
+    expect(sortedBooks[0].price).toBe(29.90);
+    expect(sortedBooks[1].price).toBe(39.90);
+    expect(sortedBooks[2].price).toBe(59.90);
   });
 
   it('should sort books by price from highest to lowest', () => {
-    const sortedBooks = sortingBooks('moreExpensive', booksData);
-    expect(sortedBooks[0].price).toBe(15);
-    expect(sortedBooks[1].price).toBe(10);
-    expect(sortedBooks[2].price).toBe(5);
+    const sortedBooks = sortingBooks('moreExpensive', bookMock);
+    expect(sortedBooks[0].price).toBe(59.90);
+    expect(sortedBooks[1].price).toBe(39.90);
+    expect(sortedBooks[2].price).toBe(29.90);
   });
 });
