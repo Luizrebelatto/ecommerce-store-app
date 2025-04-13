b2693a67bbcf0a708e2dc7009053a1f372636339
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-04-13 11:50:48 -0300

    add tests byDisplayValue

--- __tests__/inputField.test.tsx
+++ __tests__/inputField.test.tsx
@@ -22,32 +22,7 @@ describe('InputField Component', () => {
     expect(input.props.keyboardType).toBe("default");
     expect(input.props.placeholder).toBe("Enter a value");
     expect(input.props.label).toBe("Title field");
-    expect(screen.getByDisplayValue('Title field')).toBeDefined()
-
-    fireEvent.changeText(input, "New title");
-
-    expect(mockOnChangeText).toHaveBeenCalledWith("New title");
-  });
-
-  it('should render input field and k props', () => {
-    const mockOnChangeText = jest.fn();
-    
-    render(
-      <InputField
-        label='Title field'
-        value="Title Book"
-        onChangeText={mockOnChangeText}
-        placeholder="Enter a value"
-        keyboardType='default'
-      />
-    );
-
-    const input = screen.getByPlaceholderText("Enter a value");
-
-    expect(input.props.value).toBe("Title Book");
-    expect(input.props.keyboardType).toBe("default");
-    expect(input.props.placeholder).toBe("Enter a value");
-    expect(screen.getByDisplayValue('Title field')).toBeTruthy()
+    expect(screen.getByDisplayValue('Title Book')).toBeDefined()
 
     fireEvent.changeText(input, "New title");
 
